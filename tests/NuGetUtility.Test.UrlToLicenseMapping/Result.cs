5bc5dab938639287e16b63143d3cb2fc7a358221
Simon Ensslen <sensslen@gmail.com>
2025-05-12 09:11:43 +0200

    Fix test failures due to rate limiting

---
+++ tests/NuGetUtility.Test.UrlToLicenseMapping/Result.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the projects contributors.
+// The license conditions are provided in the LICENSE file located in the project root
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace NuGetUtility.Test.LicenseValidator
+{
+    public readonly struct Result<T>
+    {
+        public T Value { get; init; }
+        public string? Error { get; init; }
+        [MemberNotNullWhen(true, nameof(Value))]
+        [MemberNotNullWhen(false, nameof(Error))]
+        public bool IsSuccess => Error is null;
+    }
+}
--- tests/NuGetUtility.Test.UrlToLicenseMapping/UrlToLicenseMappingTest.cs
+++ tests/NuGetUtility.Test.UrlToLicenseMapping/UrlToLicenseMappingTest.cs
@@ -3,40 +3,64 @@
 
 using NuGetUtility.LicenseValidator;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace NuGetUtility.Test.LicenseValidator
 {
     [TestFixture]
     public class UrlToLicenseMappingTest
     {
-        [Parallelizable(scope: ParallelScope.All)]
+        private const int RETRY_COUNT = 3;
+        private DisposableWebDriver _driver = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _driver = new DisposableWebDriver();
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            _driver.Dispose();
+        }
+
         [TestCaseSource(typeof(UrlToLicenseMapping), nameof(UrlToLicenseMapping.Default))]
         public async Task License_Should_Be_Available_And_Match_Expected_License(KeyValuePair<Uri, string> mappedValue)
         {
             int retryCount = 0;
-            while (true)
+            while (retryCount < RETRY_COUNT)
             {
                 try
                 {
-                    using var driver = new DisposableWebDriver();
-                    await driver.Navigate().GoToUrlAsync(mappedValue.Key.ToString());
-
-                    await Verify(driver.FindElement(By.TagName("body")).Text).HashParameters().UseStringComparer(CompareLicense);
-                    return;
+                    Result<string> licenseResult = await GetLicenseValue(mappedValue.Key);
+                    if (licenseResult.IsSuccess)
+                    {
+                        await Verify(licenseResult.Value).HashParameters().UseStringComparer(CompareLicense);
+                        return;
+                    }
+                    await Task.Delay(500 + Random.Shared.Next(5000));
+                    await TestContext.Out.WriteLineAsync(licenseResult.Error);
                 }
                 catch (WebDriverException e)
                 {
-                    if (retryCount >= 3)
-                    {
-                        throw;
-                    }
                     retryCount++;
-                    await TestContext.Out.WriteLineAsync($"Failed to check license for the {retryCount} time - retrying");
                     await TestContext.Out.WriteLineAsync(e.ToString());
                 }
+                await TestContext.Out.WriteLineAsync($"Failed to check license for the {retryCount} time - retrying");
             }
+            Assert.Fail($"Failed to check license for {mappedValue.Key} after {RETRY_COUNT} attempts.");
+        }
 
+        private async Task<Result<string>> GetLicenseValue(Uri licenseUrl)
+        {
+            await _driver.Navigate().GoToUrlAsync(licenseUrl.ToString());
+            string bodyText = _driver.FindElement(By.TagName("body")).Text;
+            if (bodyText.Contains("rate limit"))
+            {
+                return new() { Error = $"Rate limit exceeded:\n{bodyText}" };
+            }
+            return new() { Value = bodyText };
         }
 
         private static Task<CompareResult> CompareLicense(string received, string verified, IReadOnlyDictionary<string, object> context)
@@ -50,9 +74,9 @@ namespace NuGetUtility.Test.LicenseValidator
 
             public DisposableWebDriver()
             {
-                var options = new ChromeOptions();
-                options.AddArguments("--disable-dev-shm-usage", "--headless");
-                _driver = new ChromeDriver(options);
+                var options = new FirefoxOptions();
+                options.AddArguments("--headless");
+                _driver = new FirefoxDriver(options);
             }
 
             public void Dispose()
