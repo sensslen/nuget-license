f924cd83a8b262d31ea15dcacf7928976150d8bd
Kent Chenery <kent.chenery@educationperfect.com>
2025-08-27 12:04:20 +1200

    Rebase and sonar issues

--- src/NuGetUtility/Wrapper/ZipArchiveWrapper/ZipArchiveAdapter.cs
+++ src/NuGetUtility/Wrapper/ZipArchiveWrapper/ZipArchiveAdapter.cs
@@ -7,13 +7,31 @@ namespace NuGetUtility.Wrapper.ZipArchiveWrapper;
 
 public class ZipArchiveAdapter(ZipArchive zipArchive) : IZipArchive
 {
+    private bool _disposed = false;
+
     public IZipArchiveEntry? GetEntry(string entryName)
     {
         ZipArchiveEntry? entry = zipArchive.GetEntry(entryName);
         return entry != null ? new ZipArchiveEntryWrapper(entry) : null;
     }
 
-    public void Dispose() => zipArchive.Dispose();
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                zipArchive?.Dispose();
+            }
+            _disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 }
 
 public class ZipArchiveEntryWrapper(ZipArchiveEntry entry) : IZipArchiveEntry
