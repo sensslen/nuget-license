c8abc17b483cbf3d42867bd0902d155acd6dd9af
Simon Ensslen <sensslen@gmail.com>
2026-02-09 10:36:06 +0100

    fix style

--- src/NuGetLicense/ICommandLineOptions.cs
+++ src/NuGetLicense/ICommandLineOptions.cs
@@ -3,24 +3,25 @@
 
 using NuGetUtility;
 
-namespace NuGetLicense;
-
-public interface ICommandLineOptions
+namespace NuGetLicense
 {
-    string? InputFile { get; }
-    string? InputJsonFile { get; }
-    bool IncludeTransitive { get; }
-    string? AllowedLicenses { get; }
-    string? IgnoredPackages { get; }
-    string? LicenseMapping { get; }
-    string? OverridePackageInformation { get; }
-    string? DownloadLicenseInformation { get; }
-    OutputType OutputType { get; }
-    public bool ReturnErrorsOnly { get; }
-    public bool IncludeIgnoredPackages { get; }
-    public string? ExcludedProjects { get; }
-    public bool IncludeSharedProjects { get; }
-    public string? TargetFramework { get; }
-    public string? DestinationFile { get; }
-    public string? LicenseFileMappings { get; }
+    public interface ICommandLineOptions
+    {
+        string? InputFile { get; }
+        string? InputJsonFile { get; }
+        bool IncludeTransitive { get; }
+        string? AllowedLicenses { get; }
+        string? IgnoredPackages { get; }
+        string? LicenseMapping { get; }
+        string? OverridePackageInformation { get; }
+        string? DownloadLicenseInformation { get; }
+        OutputType OutputType { get; }
+        public bool ReturnErrorsOnly { get; }
+        public bool IncludeIgnoredPackages { get; }
+        public string? ExcludedProjects { get; }
+        public bool IncludeSharedProjects { get; }
+        public string? TargetFramework { get; }
+        public string? DestinationFile { get; }
+        public string? LicenseFileMappings { get; }
+    }
 }
