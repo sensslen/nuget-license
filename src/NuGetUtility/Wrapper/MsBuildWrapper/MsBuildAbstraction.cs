5a0ee868e1b9cacdf8141bdf14a00fdaf8f7912c
Simon Ensslen <sensslen@users.noreply.github.com>
2026-06-22 08:21:11 +0200

    Simplify exception message for MSBuild detection
    Removed redundant information about MSBuild detection.

--- src/NuGetUtility/Wrapper/MsBuildWrapper/MsBuildAbstraction.cs
+++ src/NuGetUtility/Wrapper/MsBuildWrapper/MsBuildAbstraction.cs
@@ -39,8 +39,7 @@ namespace NuGetUtility.Wrapper.MsBuildWrapper
                 {
                     throw new MsBuildAbstractionException(
                         "No MSBuild instance could be detected. The .NET Framework variant of this tool requires Visual Studio or the Visual Studio Build Tools to be installed in order to locate MSBuild. " +
-                        "Please install the \"Visual Studio Build Tools\" (https://visualstudio.microsoft.com/downloads/?q=build+tools) or use the .NET Core (dotnet tool) variant instead. " +
-                        "See https://learn.microsoft.com/visualstudio/msbuild/updating-an-existing-application for more information on how MSBuild is detected.");
+                        "Please install the \"Visual Studio Build Tools\" (https://visualstudio.microsoft.com/downloads/?q=build+tools) or use the .NET Core (dotnet tool) variant instead. ");
                 }
 
                 MSBuildLocator.RegisterInstance(instance);
